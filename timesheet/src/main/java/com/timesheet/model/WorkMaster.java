5886ecba25af5d645f676b7fa7773f400076eb93
shivam219 <61514793+shivam219@users.noreply.github.com>
2022-10-23 16:39:06 +0530

    Update WorkMaster.java

--- timesheet/src/main/java/com/timesheet/model/WorkMaster.java
+++ timesheet/src/main/java/com/timesheet/model/WorkMaster.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 public class WorkMaster {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.TABLE)
 	@Column(name = "id")
 	Long id;
 	@Column(name = "emp_id")
